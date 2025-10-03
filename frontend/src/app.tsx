153affc44c5c11edd166401b3a48d3bfdbfc7fad
vunam29100 <namvhph39762@fpt.edu.vn>
2025-10-03 22:21:17 +0700

    giao dien thong tin nguoi dung

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,15 +1,6 @@
 import { Navigate, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-<<<<<<< HEAD
-import ShopPage from "./pages/ShopPage";
-import ProductDetailsPage from "./pages/ProductDetailsPage";
-import CartPage from "./pages/CartPage";
-import UserPage from "./pages/UserPage";
-import Login from "./pages/Auth/Login/login";
-import Register from "./pages/Auth/Register/register";
-import Auth from "./pages/Auth/page";
-=======
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Blog from "./pages/Blog";
@@ -18,8 +9,9 @@ import ShopDetail from "./pages/ShopDetail";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Cart from "./pages/Cart";
+import ProfileUser from "./pages/ProfileUser/Page";
+import UserAccount from "./pages/ProfileUser/profile/UserAccount";
 
->>>>>>> 694bd7caf5b0245dbfa315571f43906ddf909e1b
 function App() {
   const isLoggedIn = () => {
     return !!localStorage.getItem("token");
@@ -29,35 +21,6 @@ function App() {
       <Header />
       <main className="flex-1 pt-20">
         <Routes>
-<<<<<<< HEAD
-          <Route path="/" element={<ShopPage />} />
-          <Route path="/shop" element={<ShopPage />} />
-          <Route path="/product/:id" element={<ProductDetailsPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/user" element={<UserPage />} />
-          <Route path="/auth" element={<Auth />}>
-            <Route
-              index
-              element={
-                isLoggedIn() ? (
-                  <Navigate to="/" replace />
-                ) : (
-                  <Navigate to="login" replace />
-                )
-              }
-            />
-            <Route
-              path="login"
-              element={isLoggedIn() ? <Navigate to="/" replace /> : <Login />}
-            />
-            <Route
-              path="register"
-              element={
-                isLoggedIn() ? <Navigate to="/" replace /> : <Register />
-              }
-            />
-          </Route>
-=======
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/shop/:id" element={<ShopDetail />} />
@@ -66,7 +29,12 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/cart" element={<Cart />} />
->>>>>>> 694bd7caf5b0245dbfa315571f43906ddf909e1b
+          {/* ProfileUser */}
+          <Route path="/account" element={<ProfileUser />}>
+            <Route index element={<Navigate to="profile" replace />} />
+            <Route path="profile" element={<UserAccount />} />
+          </Route>
+        </Routes>
         </Routes>
       </main>
       <Footer />
