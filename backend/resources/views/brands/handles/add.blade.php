32ad3d4e561f02573b1c3002c13df01995e3bff8
thangne00 <thangndph37125@fpt.edu.vn>
2025-10-05 10:31:04 +0700

    cap nhat ve giao dien quan li danh muc sp

---
+++ backend/resources/views/brands/handles/add.blade.php
@@ -0,0 +1,51 @@
+
+<script>
+    $(document).ready(function(){
+
+        $(".form-add").submit(function(){
+            event.preventDefault();
+            let _token = $("input[name='_token'").val()
+            let data =  new FormData();
+            const inputs = Array.from(this.querySelectorAll(".form-control"));
+            inputs.forEach(function(input){
+                    data.append(input.name,input.value.trim());
+
+            })
+            data.append("_token",_token);
+
+            $.ajax({
+            url : '{{route('admin.brand.store')}}',
+            type: "POST",
+            dataType: "json",
+            data : data,
+            contentType: false,
+            processData: false,
+            success : function(res){
+                toastMessage(res[1],res[0],'{{route('admin.brand')}}')
+
+            },
+            error : function(error){
+            let errors =  error.responseJSON.errors;
+            document.querySelectorAll(".message-error").forEach(function(e){
+                e.innerText = "";
+            })
+                Object.keys(errors).forEach(function(error){
+                    const input = document.querySelector(`input[name="${error}"]`);
+                    const select = document.querySelector(`select[name="${error}"]`)
+                    if(input){
+                    const message = input.parentElement.querySelector("p");
+                        message.innerText = errors[error]
+
+                    }
+                    if(select){
+                        const message = select.parentElement.querySelector(".message-error");
+
+                        message.innerText = errors[error]
+
+                    }
+                })
+            }
+        })
+        })
+    })
+</script>
