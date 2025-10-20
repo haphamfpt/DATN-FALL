99f15215bb7cf044d156557528d5d0ebd03ee4da
tuanpaph <tuanpaph31233@fpt.edu.vn>
2025-10-20 19:26:21 +0700

    hien thị danh mục

---
+++ backend/app/Models/Category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $table = 'categories';
+    protected $primaryKey = 'category_id';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'category_name',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'category_id', 'category_id');
+    }
+}
