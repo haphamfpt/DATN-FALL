0d6c9342bec31e67ba4b16b4068ff42aecc08bd6
thangne00 <thangndph37125@fpt.edu.vn>
2025-10-25 17:53:14 +0700

    Models update Products

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'product';
+
+    protected $primaryKey = 'product_id';
+
+    public $timestamps = true;
+
+    protected $fillable = [
+       
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, '');
+    }
+
+    public function variants()
+    {
+        return $this->hasMany(Variant::class, '');
+    }
 }
